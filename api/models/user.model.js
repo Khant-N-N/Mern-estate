297705acbf7bd00fcbb2914ebffe29a564f34dc5
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-06 00:22:36 +0630

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    username: {
+        type: String,
+        required: true,
+    }
+}, { timestamps: true }); //auto sort latest time
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
