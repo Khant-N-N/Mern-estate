5a40476895810cbfcb18d7b450bc168258cc7a01
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-06 02:07:33 +0630

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js"
+import authRouter from "./routes/auth.route.js"
 
 dotenv.config();
 const app = express();
@@ -11,6 +13,10 @@ mongoose.connect(process.env.MONGO).then(() => {
     console.log("error connecting mongodb", err)
 })
 
+app.use(express.json()); // by default json is not allow to the server, so allow json as input
 app.listen(3001, () => {
     console.log("Server is started")
 })
+app.use("/api/user", userRouter)
+
+app.use("/api/auth", authRouter)
