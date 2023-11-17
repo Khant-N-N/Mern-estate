7c3300bb468b538f713373e7a94d1192979bf825
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-18 01:19:17 +0630

    list details ui added

--- client/src/pages/ListDetails.jsx
+++ client/src/pages/ListDetails.jsx
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 import Loader from "../components/Loader";
 import { IoLocation } from "react-icons/io5";
 import { FaBed, FaBath, FaChair } from "react-icons/fa6";
-import { FaCar } from "react-icons/fa";
+import { FaCar, FaShare } from "react-icons/fa";
 import { Swiper, SwiperSlide } from "swiper/react";
 import SwiperCore from "swiper";
 import { Navigation } from "swiper/modules";
@@ -15,6 +15,7 @@ const ListDetails = () => {
   const [detail, setDetail] = useState(null);
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [copy, setCopy] = useState(false);
   console.log(detail);
   useEffect(() => {
     const getDetail = async () => {
@@ -35,7 +36,12 @@ const ListDetails = () => {
     getDetail();
   }, [id]);
   return (
-    <main>
+    <main className="relative">
+      {error && (
+        <p className="text-red-500 flex items-center justify-center h-[60vh]">
+          Something went wrong!
+        </p>
+      )}
       {loading ? (
         <Loader />
       ) : (
@@ -51,10 +57,41 @@ const ListDetails = () => {
               </SwiperSlide>
             ))}
           </Swiper>
-
+          <div
+            title="share"
+            onClick={() => {
+              setCopy(true);
+              navigator.clipboard.writeText(window.location.href);
+              setTimeout(() => {
+                setCopy(false);
+              }, 600);
+            }}
+            className="p-4 bg-[--sec] absolute z-10 right-5 top-10 rounded-full cursor-pointer hover:opacity-50"
+          >
+            <FaShare />
+          </div>
+          <p
+            className={`${
+              copy ? "scale-100" : "scale-0"
+            } transition-all absolute top-24 right-3 z-10 bg-[var(--sec)] p-3 rounded`}
+          >
+            Link Copied
+          </p>
           <div className="w-full max-w-[800px] mx-auto p-5">
             <h2 className="font-bold my-3 text-[24px]">
-              {detail?.name} - <span>$ {detail?.regularPrice}</span>
+              {detail?.name} -{" "}
+              <span className={`${detail?.offer && "line-through"}`}>
+                $ {detail?.regularPrice.toLocaleString("en-US")}
+              </span>{" "}
+              {detail?.offer && (
+                <span>
+                  ${" "}
+                  {(
+                    detail?.regularPrice - detail?.discountPrice
+                  ).toLocaleString("en-US")}
+                </span>
+              )}
+              {detail?.type === "rent" && "/ month"}
             </h2>
             <p className="text-[var(--text2)] text-[17px]">
               <IoLocation className="inline text-green-600 mb-1" />{" "}
@@ -62,32 +99,40 @@ const ListDetails = () => {
             </p>
             <button
               type="button"
-              className="px-8 py-1 my-3 capitalize bg-red-500 rounded-md"
+              className="px-8 py-1 my-3 capitalize bg-red-500 rounded-md mr-3 text-[17px] text-white"
             >
               For {detail?.type}
             </button>
+            {detail?.offer && (
+              <button
+                type="button"
+                className="px-8 py-1 my-3 capitalize bg-green-500 rounded-md text-[17px] text-white"
+              >
+                $ {detail?.discountPrice} discount
+              </button>
+            )}
             <p className="text-[17px] my-5">
               <span className="font-bold">Description - </span>{" "}
               <span>{detail?.description}</span>
             </p>
-            <div className="flex flex-wrap items-center gap-5">
-              {detail?.bedroooms > 0 && (
-                <p className="flex items-center gap-3 text-green-500 text-[16px]">
-                  <FaBed /> {detail?.bedroooms} bedroooms
+            <div className="flex flex-wrap items-center gap-5 text-green-500 text-[16px] font-semibold">
+              {detail?.bedrooms > 0 && (
+                <p className="flex items-center gap-3">
+                  <FaBed /> {detail?.bedrooms} bedroooms
                 </p>
               )}
               {detail?.bathrooms > 0 && (
-                <p className="flex items-center gap-3 text-green-500 text-[16px]">
+                <p className="flex items-center gap-3">
                   <FaBath /> {detail?.bathrooms} bathroooms
                 </p>
               )}
               {detail?.furnished && (
-                <p className="flex items-center gap-3 text-green-500 text-[16px]">
+                <p className="flex items-center gap-3">
                   <FaChair /> furnished
                 </p>
               )}
               {detail?.parking && (
-                <p className="flex items-center gap-3 text-green-500 text-[16px]">
+                <p className="flex items-center gap-3 ">
                   <FaCar /> Parking
                 </p>
               )}
