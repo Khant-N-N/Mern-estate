9ce9f81defddd220a78c7db9b8e2d994638c6baa
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-19 02:40:48 +0630

    search api route

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,4 +1,4 @@
-import Listing from "../models/listing.model.js"
+import Listing from "../models/listing.model.js";
 import { errorHandler } from "../utils/error.js";
 
 export const createlisting = async (req, res, next) => {
@@ -39,6 +39,52 @@ export const updateListing = async (req, res, next) => {
     try {
         const updateList = await Listing.findByIdAndUpdate(req.params.id, req.body, { new: true });
         res.status(201).json(updateList);
+    } catch (error) {
+        next(error)
+    }
+}
+
+export const getListings = async (req, res, next) => {
+    try {
+        //query come from url (isting/getlist?limit=2&blah=blah)
+        const limit = parseInt(req.query.limit) || 9 //u can add limit when req
+        const startIndex = parseInt(req.query.startIndex) || 0
+
+        let offer = req.query.offer;
+        if (offer === undefined || offer === "false") {
+            offer = { $in: [false, true] } // $in is (search inside a database) with all false and true values
+        }
+
+        let furnished = req.query.furnished;
+        if (furnished === undefined || furnished === "false") furnished = { $in: [false, true] }
+
+        let parking = req.query.parking;
+        if (parking === undefined || parking === "false") parking = { $in: [false, true] }
+
+        let type = req.query.type;
+
+        if (type === undefined || type === "all") type = { $in: ['sale', 'rent'] }
+
+        const searchTerm = req.query.searchTerm || "";
+
+        const sort = req.query.sort || "createdAt";
+
+        const order = req.query.sort || "desc";
+
+        const getListing = await Listing.find({
+            //option is for not care about upper/lower cases
+            name: { $regex: searchTerm, $options: "i" }, //regex is for building search functional for mongodb(can be search with some letter part of words)
+            offer,
+            parking,
+            furnished,
+            type,
+        }).sort({
+            [sort]: order
+        }).limit(limit).skip(startIndex);
+
+        return res.status(200).json(getListing);
+
+
     } catch (error) {
         next(error)
     }
--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createlisting, deleteUserListings, getDetail, updateListing } from "../controllers/listing.controller.js";
+import { createlisting, deleteUserListings, getDetail, updateListing, getListings } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/create", verifyToken, createlisting);
 router.delete("/delete/:id", verifyToken, deleteUserListings);
 router.get("/detail/:id", getDetail);
-router.post("/update/:id", verifyToken, updateListing)
+router.post("/update/:id", verifyToken, updateListing);
+router.get("/getlist", getListings)
 
 export default router;
