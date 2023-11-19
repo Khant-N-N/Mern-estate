cb5f40e430188747d26813ee53f1b5dcb78277db
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-20 03:10:17 +0630

    replace ref with state to update input value

--- client/src/components/SendMessage.jsx
+++ client/src/components/SendMessage.jsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
 const SendMessage = ({ detail }) => {
   const [landlord, setLandlord] = useState(null);
-  const messageRef = useRef("");
+  const [message, setMessage] = useState("");
   useEffect(() => {
     const getUser = async () => {
       try {
@@ -24,14 +24,15 @@ const SendMessage = ({ detail }) => {
         <span className="font-bold">{detail?.name}</span>
       </h2>
       <textarea
-        ref={messageRef}
+        value={message}
+        onChange={(e) => setMessage(e.target.value)}
         spellCheck={false}
         name="message"
         placeholder="Message"
         className="w-full border-none outline-none shadow p-3 my-6 h-24 rounded"
       ></textarea>
       <Link
-        to={`mailto:${landlord?.email}?subject=Regarding ${detail.name}&body=${messageRef.current.value}`}
+        to={`mailto:${landlord?.email}?subject=Regarding ${detail.name}&body=${message}`}
         className="p-3 bg-[var(--text2)] text-center rounded my-6 text-white hover:opacity-70 uppercase"
       >
         SEND MESSAGE
