71d2845ad5608bf3b0d2dec0ff3c6464ab860613
Khant Nyi Nyi <khantnyinyi.magnet@gmail.com>
2023-11-19 16:16:50 +0630

    hamburger menu add

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,10 +1,12 @@
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { FaSearch } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi";
 import { useEffect, useState } from "react";
 const Navbar = () => {
   const { currentUser } = useSelector((state) => state.user);
   const [searchTerm, setSearchTerm] = useState("");
+  const [menu, setMenu] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -27,7 +29,7 @@ const Navbar = () => {
     }
   };
   return (
-    <nav className="flex justify-around items-center py-3 bg-[var(--sec)]">
+    <nav className="flex justify-around items-center py-3 bg-[var(--sec)] z-40">
       <Link to="/" className="text-[var(--text1)]">
         Logo
       </Link>
@@ -38,17 +40,49 @@ const Navbar = () => {
           type="text"
           name="searchbox"
           placeholder="search"
-          className="border-none outline-none py-2 px-3 w-24 sm:w-64 rounded-lg text-[16px]"
+          className="border-none outline-none py-2 px-3 w-36 sm:w-64 rounded-lg text-[16px]"
         />
         <button type="submit">
           <FaSearch className="absolute right-3 top-[0.7rem]" />
         </button>
       </form>
-      <div className="flex gap-3">
+      <div className="flex gap-3 items-center">
+        <div className="relative md:hidden cursor-pointer">
+          <GiHamburgerMenu
+            className="text-[22px]"
+            onClick={() => setMenu(!menu)}
+          />
+          <div
+            className={`${
+              menu ? "bottom-[-7rem]" : "bottom-0"
+            } flex flex-col gap-3 z-[-1] absolute right-0 bg-[--sec] p-3 rounded transition-all`}
+          >
+            <NavLink
+              to="/"
+              onClick={() => setMenu(false)}
+              className={({ isActive }) =>
+                isActive ? "text-[var(--text1)]" : "text-[var(--text2)]"
+              }
+            >
+              Home
+            </NavLink>
+            <NavLink
+              to="/about"
+              onClick={() => setMenu(false)}
+              className={({ isActive }) =>
+                isActive ? "text-[var(--text1)]" : "text-[var(--text2)]"
+              }
+            >
+              About
+            </NavLink>
+          </div>
+        </div>
         <NavLink
           to="/"
           className={({ isActive }) =>
-            isActive ? "text-[var(--text1)]" : "text-[var(--text2)]"
+            isActive
+              ? "hidden md:inline text-[var(--text1)]"
+              : "hidden md:inline text-[var(--text2)]"
           }
         >
           Home
@@ -56,7 +90,9 @@ const Navbar = () => {
         <NavLink
           to="/about"
           className={({ isActive }) =>
-            isActive ? "text-[var(--text1)]" : "text-[var(--text2)]"
+            isActive
+              ? "hidden md:inline text-[var(--text1)]"
+              : "hidden md:inline text-[var(--text2)]"
           }
         >
           About
